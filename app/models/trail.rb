404ab013f58d31235863f085c528878844b4468d
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-07-12 15:01:26 -0700

    add basic styles

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,4 @@
 class Trail < ApplicationRecord
   validates :name, presence: true
-  validates :description, presence: true, length: { maximum: 140 }
+  validates :description, presence: true, length: { maximum: 500 }
 end
